1ac2ad6f48ecd7a77546c07402e501ceedc7b698
Jeffrey Hu <zhiwehu@gmail.com>
2014-08-05 08:49:32 +0800

    gunicorn_start.bash

---
+++ gunicorn_start.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NAME="lettoo_weixin_platform"                                  # Name of the application
+DJANGODIR=/home/ecs-user/lettoo-weixin-platform-back/src             # Django project directory
+SOCKFILE=/home/ecs-user/webapp/run/gunicorn.sock  # we will communicte using this unix socket
+USER=ecs-user                                        # the user to run as
+GROUP=ecs-user                                     # the group to run as
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=core.settings.production             # which settings file should Django use
+DJANGO_WSGI_MODULE=core.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source /home/ecs-user/env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+#export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=debug \
+  --bind=unix:$SOCKFILE
