de7218c8c012bebd78dd7e3cef0a14ab54e24aa2
Jeffrey Hu <zhiwehu@gmail.com>
2014-08-11 13:31:44 +0800

    added supervisor

--- gunicorn_start.bash
+++ gunicorn_start.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-NAME="lettoo_weixin_platform"                                  # Name of the application
+NAME="lettoo_weixin_platform"                                        # Name of the application
 DJANGODIR=/home/ecs-user/lettoo-weixin-platform-back/src             # Django project directory
-SOCKFILE=/home/ecs-user/webapp/run/gunicorn.sock  # we will communicte using this unix socket
-USER=ecs-user                                        # the user to run as
-GROUP=ecs-user                                     # the group to run as
-NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=core.settings.production             # which settings file should Django use
-DJANGO_WSGI_MODULE=core.wsgi                     # WSGI module name
+SOCKFILE=/home/ecs-user/gunicorn.sock                                # we will communicte using this unix socket
+USER=ecs-user                                                        # the user to run as
+GROUP=ecs-user                                                       # the group to run as
+NUM_WORKERS=3                                                        # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=core.settings.production                      # which settings file should Django use
+DJANGO_WSGI_MODULE=core.wsgi                                         # WSGI module name
 
 echo "Starting $NAME as `whoami`"
 
@@ -15,7 +15,7 @@ echo "Starting $NAME as `whoami`"
 cd $DJANGODIR
 source /home/ecs-user/env/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-#export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
@@ -23,7 +23,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec /home/ecs-user/env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
