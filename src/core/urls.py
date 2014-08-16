62518b12a2b44077b1971f7ee5697ddbcebcd56e
Jeffrey Hu <zhiwehu@gmail.com>
2014-08-16 09:04:15 +0800

    redirect / to /app/

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,7 +8,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       #url(r'^$', TemplateView.as_view(template_name='base.html')),
+                       url(r'^$', RedirectView.as_view(url='/app/')),
 
                        url(r'^api/', include('api.urls')),
                        url(r'^account/', include('account.urls')),
