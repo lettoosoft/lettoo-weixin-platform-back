f5c26dd7b87692ea6a75e5f45759ea81c4cf2298
Jeffrey Hu <zhiwehu@gmail.com>
2014-08-17 22:46:50 +0800

    added weixin url

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
 
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
+
+                       url(r'^', include('weixin.urls')),
 )
 
 # Uncomment the next line to serve media files in dev.
