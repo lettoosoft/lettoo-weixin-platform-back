d3034175c13b7991303b48c6f72a5d01ad248884
Jeffrey Hu <zhiwehu@gmail.com>
2014-08-25 15:49:23 +0800

    增加WeixinAppResource api url

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,6 +4,7 @@ from tastypie.api import Api
 from .v1.user import UserResource, CreateUserResource
 from .v1.weixin_resource import PublicAccountResource
 from .v1.attachment_resource import AttachmentResource
+from .v1.weixinapp_resource import WeixinAppResource
 
 
 v1_api = Api(api_name='v1')
@@ -11,6 +12,7 @@ v1_api.register(UserResource())
 v1_api.register(CreateUserResource())
 v1_api.register(PublicAccountResource())
 v1_api.register(AttachmentResource())
+v1_api.register(WeixinAppResource())
 
 urlpatterns = patterns('',
                        url(r'', include(v1_api.urls)),
